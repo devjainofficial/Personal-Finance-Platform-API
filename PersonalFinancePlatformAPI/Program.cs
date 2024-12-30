fc98dfbc24a57b1d201837bb2e0f1445d18b14f5
devjainofficial <devjainofficials@gmail.com>
2024-12-30 23:02:44 +0530

    Added CORS to connect with UI Side

--- PersonalFinancePlatformAPI/Program.cs
+++ PersonalFinancePlatformAPI/Program.cs
@@ -3,8 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options => 
+{ 
+    options.AddPolicy
+    ("AllowAngularApp", policy => policy.WithOrigins("http://localhost:4200")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod()); 
+    }
+);
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -19,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAngularApp");
 
 app.MapControllers();
 
